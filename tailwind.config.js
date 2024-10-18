7fc7d4c390dddf074cbadad93a5cf9d7972da82a
apustovoitova <anastasiapustovoitova@gmail.com>
2024-10-18 12:49:17 +0200

    created mobile menu btns on 768px : hamburger menu, close btn; added folder plugins; addEventListener "click"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const openVariant = require("./plugins/openVariant");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("./plugins/openVariant")],
 };
