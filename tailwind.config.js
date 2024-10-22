93ec2451b2e030d52f25e9dc3b8fb412093da6d8
apustovoitova <anastasiapustovoitova@gmail.com>
2024-10-22 23:34:37 +0200

    created custom animation delay plugin with matchUtilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,5 +21,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("./plugins/openVariant")],
+  plugins: [
+    require("./plugins/openVariant"),
+    require("./plugins/animationDelay"),
+  ],
 };
